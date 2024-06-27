0261c1b6a942d4ffc1614084f421fe66caf70a5b
Heberth López <heberth.lopez.19@gmail.com>
2024-06-27 18:44:40 -0500

    Added demo data for Client

--- VideoTienda/Controllers/ClienteController.cs
+++ VideoTienda/Controllers/ClienteController.cs
@@ -7,7 +7,12 @@ namespace VideoTienda.Controllers;
 [Route("api/[controller]")]
 public class ClienteController: ControllerBase
 {
-    private static readonly List<Cliente> Clientes = new List<Cliente>();
+    private static readonly List<Cliente> Clientes = new List<Cliente>
+    {
+        new Cliente(){Id = 1, Nombre = "Testino Probino", Email = "testino@mail.com"},
+        new Cliente(){Id = 2, Nombre = "John Doe", Email = "johndoe@mail.com"},
+        new Cliente(){Id = 3, Nombre = "John Snow", Email = "johnsnow@mail.com"},
+    };
     
     // GET: api/cliente
     [HttpGet]
